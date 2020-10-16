2dae3050870a76887de9a0177fd0c97cd05caa1f
Sufiaqilah <63999801+Sufiaqilah@users.noreply.github.com>
2020-10-16 22:44:10 +0800

    Add files via upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'RotexSolution') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+    <style>
+        .masthead {
+            background-image: url('https://images.unsplash.com/photo-1558898268-92ae44e7670e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+        }
+    </style>
+</head>
+<body>
+    <div id="app">
+        @include('inc.navbar')
+        <main>
+            @include('inc.messages')
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
+
+
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+
+@section('content')
+    <header class="masthead">
+        <div class="container">
+            <div class="masthead-subheading">Welcome To RotexSolution</div>
+            <div class="masthead-heading text-uppercase">Renew your road-tax online!</div>
+        </div>
+        @guest
+            <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+
+            @else
+                @if (Auth::user()->role == "admin")
+                    <a class="btn btn-primary btn-lg" href="/admins" role="button">Go to Admin panel</a>
+
+                    @else
+                        <a class="btn btn-primary btn-lg" href="/dashboard" role="button">Go to your dashboard</a>
+                @endif
+
+        @endguest
+    </header>
+
+    <!-- Services-->
+    <section class="page-section" id="services">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">Services</h2>
+                <h3 class="section-subheading text-muted">We offer you with quality services</h3>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <h4 class="my-3">Digital bluecard</h4>
+                    <p class="text-muted">Upload your bluecard and make it digital! Now you only have to present your QR Code to review the bluecard.</p>
+                </div>
+                <div class="col-md-4">
+                    <h4 class="my-3">Renew your Road-tax with us</h4>
+                    <p class="text-muted">Renewing never been this easier. Submit your digital bluecard and insurance to us, make the payment then done!</p>
+                </div>
+                <div class="col-md-4">
+                    <h4 class="my-3">Pay it Online!</h4>
+                    <p class="text-muted">Pay for your renewal online! Eliminate the hassle to go and queueing for making the payment.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="page-section">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">About Us</h2>
+                <h3 class="section-subheading text-muted">We are a company with groups of people that are passionate to further improve our country's digital technology. Rotexsolution is an initiative to enhance the car's road-tax system into an online platform with better efficiency, catchy and simplicity.</h3>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer-->
+    <footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 text-lg-left">Copyright © {{ config('app.name', 'RotexSolution') }} 2020</div>
+                <div class="col-lg-6 text-lg-right">
+                    {{-- <a class="mr-3" href="#!">Privacy Policy</a>
+                    <a href="#!">Terms of Use</a> --}}
+                </div>
+            </div>
+        </div>
+    </footer>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5f8837c02901b9207693b03b/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+@endsection
